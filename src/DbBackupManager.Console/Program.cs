82b38dd449d859564807b82e63444fe0c52974ab
David Ledvinka <ledvinka.david@gmail.com>
2019-09-28 19:58:02 +0200

    initial

---
+++ src/DbBackupManager.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DbBackupManager.Core;
+
+namespace DbBackupManager.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            var bs = new BackupService
+        }
+    }
+}
