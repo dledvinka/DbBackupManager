2903835ad2c632ffb4b25f9c84ee79040516b16d
David Ledvinka <David.Ledvinka@cz.abb.com>
2019-10-01 12:51:02 +0200

    reading backup json file

--- src/DbBackupManager.Console/Program.cs
+++ src/DbBackupManager.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using DbBackupManager.Core;
 
 namespace DbBackupManager.Console
@@ -8,7 +9,12 @@ namespace DbBackupManager.Console
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
-            var bs = new BackupService("", "");
+            var bs = new BackupService(@"Data Source=.\SQLEXPRESS;Initial Catalog=SafetyDesign_Dev;Integrated Security=True", @"D:\_TEMP\DbBackupManager");
+            var dbs = bs.GetAllUserDatabases().ToList();
+            dbs.ForEach(db => System.Console.WriteLine(db));
+
+            var br = new BackupRepository(@"d:\_TEMP\DbBackupManager\_backups.json");
+            var backups = br.GetAll();
         }
     }
 }
